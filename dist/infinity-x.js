4bdad16467592fb73c9af7828e3fac1ad43bd8a3
Graham Fairweather <xotic750@gmail.com>
2019-07-13 03:56:47 +0200

    :bookmark: v2.0.2
    Test auto build

--- dist/infinity-x.js
+++ dist/infinity-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-12T12:39:58.676Z",
+  "date": "2019-07-13T01:56:42.966Z",
   "describe": "",
   "description": "The constant value Infinity.",
   "file": "infinity-x.js",
   "hash": "1cf5a9f5e9548c74d2a4",
   "license": "MIT",
-  "version": "2.0.1"
+  "version": "2.0.2"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
