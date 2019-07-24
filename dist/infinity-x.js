79289b5b3b7e1fefe674f63fb4e31df14136080b
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:07:34 +0200

    :bookmark: v2.0.21
    Add jsdelivr

--- dist/infinity-x.js
+++ dist/infinity-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-23T23:27:24.883Z",
+  "date": "2019-07-24T13:07:29.251Z",
   "describe": "",
   "description": "The constant value Infinity.",
   "file": "infinity-x.js",
   "hash": "1cf5a9f5e9548c74d2a4",
   "license": "MIT",
-  "version": "2.0.20"
+  "version": "2.0.21"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
