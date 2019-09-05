1617280d0a2d7403fcb2c0a80a92f3e45d5057de
Graham Fairweather <xotic750@gmail.com>
2019-09-05 20:22:04 +0200

    Maintenance

--- dist/infinity-x.js
+++ dist/infinity-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-09-04T22:18:59.588Z",
+  "date": "2019-09-05T18:21:59.561Z",
   "describe": "",
   "description": "The constant value Infinity.",
   "file": "infinity-x.js",
